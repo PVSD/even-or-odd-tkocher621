458768f2b39bb1ca30e758c7232f47f69c57d137
Todd <tk304@pvsd.org>
2018-09-13 21:06:19 -0400

    egg beater

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
+    public static boolean IsEven(int number)
+    {
+        if (number % 2 == 0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        System.out.println("Enter an integer");
+        num = scan.nextInt();
+
+        if (IsEven(num))
+        {
+            System.out.println(num + " is even.");
+        }
+        else
+        {
+            System.out.println(num + " is odd");
+        }
     }
 }
